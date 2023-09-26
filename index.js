b84e45f9e93050443853ad8c27d3ffbf7f758564
Cash Myers <cmyers880@gmail.com>
2023-09-26 02:18:53 -0500

    fix: Mongo connection command

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authRoutes = require('./routes/auth');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
 // Middleware
@@ -19,9 +18,9 @@ app.use(cors());
 app.use(express.json());
 
 // TODO: Add Routes
-app.use('/auth', authRoutes )
+app.use("/auth", authRoutes);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
